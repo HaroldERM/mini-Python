a50aa2d5eb7ab6dec027a891916c6bf3c779d612
Harold <61435652+HaroldERM@users.noreply.github.com>
2022-03-16 22:26:03 -0600

    Commit Harold

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+from miniPythonLexer import *
+from miniPythonParser import *
+from antlr4 import *
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def main():
+    input = FileStream('test.txt')
+    lexer = miniPythonLexer(input)
+    stream = CommonTokenStream(lexer)
+    parser = miniPythonParser(stream)
+    tree = parser.program()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
